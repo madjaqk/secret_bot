4891cf409d9c77617a56ce180190d7d8d78d63a1
Jack Brounstein <Jack.Brounstein@yahoo.com>
2017-01-23 15:42:56 -0800

    adds puzzle tracking

--- secret_bot.py
+++ secret_bot.py
@@ -15,12 +15,89 @@ BOT_ID = os.environ["SECRET_BOT_SLACK_ID"]
 sc = SlackClient(API_TOKEN)
 
 def listen_for_text():
+	"""
+	!solved Puzzlename => :confetti_ball: *Puzzlename*  was solved! :tada:
+	!solved Puzzlename SOLUTION => :confetti_ball: *Puzzlename*  was solved! :tada:  and the answer is *SOLUTION*
+	!almost HardPuzzle => Thanks.  I'll remember that we're very close on *HardPuzzle*.
+	!almost NastyPuzzle => Thanks.  I'll remember that we're very close on *NastyPuzzle*.
+	!almostpuzzles => We're currently very close on the following: *HardPuzzle*, *NastyPuzzle*.
+	!solved HardPuzzle HARDANSWER => :confetti_ball: *HardPuzzle*  was solved! :tada:  and the solution is *HARDANSWER* 
+	!almostpuzzles => We're currently very close on the following: *NastyPuzzle*.
+
+	you can actually replace these puzzle names with #puzzlechannel names.
+
+	also another idea for !almostpuzzles is !whatshouldiworkon
+	or if you're feeling memetic, !whatsgoingon
+	"""
+
+	close_puzzles = set()
+
+	SOLVED_RE = re.compile(r"^(<@{}> )?!solved ?(?P<puzzle_name>\S+)? ?(?P<solution>.+)?$".format(BOT_ID))
+	ALMOST_RE = re.compile(r"^(<@{}> )?!almost( (?P<puzzle_name>\S+))?$".format(BOT_ID))
+	ALMOST_PUZZLES_RE = re.compile(r"^(<@{}> )?!(almostpuzzles|whatshouldiworkon|whatsgoingon|close|whatsclose)$".format(BOT_ID))
+	HELP_RE = re.compile(r"^(<@{}> )?!help$")
+
 	while True:
 		data = sc.rtm_read()
 		if data:
 			activity = data[0]
 			try:
 				if activity["type"] == "message" and activity["user"] != BOT_ID:
+					print(activity)
+
+					is_solved = SOLVED_RE.match(activity["text"])
+					almost = ALMOST_RE.match(activity["text"])
+					almost_puzzles = ALMOST_PUZZLES_RE.match(activity["text"])
+					help = HELP_RE.match(activity["text"])
+					message = None
+
+					if is_solved:
+						if is_solved.group("puzzle_name"):
+							puzzle_name = is_solved.group("puzzle_name")
+						else:
+							channel_res = sc.api_call("channels.info", channel=activity["channel"])
+							if channel_res["ok"]:
+								puzzle_name = channel_res["channel"]["name"]
+							else:
+								continue
+						if is_solved.group("solution"):
+							solution = re.sub(r"[^A-Z]", "", is_solved.group("solution").upper())
+							message = ":confetti_ball: *{}*  was solved! :tada:  and the answer is *{}*".format(puzzle_name, solution)
+						else:
+							message = ":confetti_ball: *{}*  was solved! :tada:".format(puzzle_name)
+						# print(activity["channel"])
+						if activity["channel"] != "C3N1ZQWP2":
+							sc.api_call("chat.postMessage", text=message, channel="#general", link_names=1, as_user=True)
+						close_puzzles.discard(puzzle_name.title())
+					elif almost:
+						if almost.group("puzzle_name"):
+							puzzle_name = almost.group("puzzle_name")
+						else:
+							channel_res = sc.api_call("channels.info", channel=activity["channel"])
+							if channel_res["ok"]:
+								puzzle_name = channel_res["channel"]["name"]
+							else:
+								continue
+						message = "Thanks.  I'll remember that we're very close on *{}*.".format(puzzle_name)
+						close_puzzles.add(puzzle_name.title())
+					elif almost_puzzles:
+						if close_puzzles:
+							message = "We're currently very close on the following: "+ ", ".join("*{}*".format(puzz) for puzz in close_puzzles)
+						else:
+							message = "We currently have no puzzles marked as close."
+					elif help:
+						message = [
+							'@simple_bot is here to help!  Usage:',
+							'`!solved Puzzlename` => ":confetti_ball: *Puzzlename*  was solved! :tada:"',
+							'`!solved Puzzlename SOLUTION` => ":confetti_ball: *Puzzlename*  was solved! :tada:  and the answer is *SOLUTION*"',
+							'`!almost HardPuzzle` => "Thanks.  I\'ll remember that we\'re very close on *HardPuzzle*."',
+							'`!almostpuzzles` => "We\'re currently very close on the following: *Hardpuzzle*, *Nastypuzzle*." _(also !whatshouldiworkon, !whatsgoingon, !close, and !whatsclose)_ ',
+						]
+						message = "\n".join(message)
+						
+					if message:
+						sc.api_call("chat.postMessage", text=message, channel=activity["channel"], link_names=1, as_user=True)
+
 					if "secret" in activity["text"]:
 						with open("LOZ_Secret.wav", "rb") as f:
 							sc.api_call("files.upload", filename="LOZ_Secret.wav", file=f, channels=activity["channel"])
@@ -47,7 +124,13 @@ def check_for_channels():
 				for channel in api_res["channels"]:
 					if channel["id"] not in channels:
 						try:
-							sc.api_call("chat.postMessage", text="New channel: #{}".format(channel["name"]), channel="#general", link_names=1, as_user=True)
+							user_res = sc.api_call("users.info", user=channel["creator"])
+							if user_res["ok"]:
+								message = "@{} created the channel #{}  Don't forget to `/invite @simple_bot`!".format(user_res["user"]["name"], channel["name"])
+							else:
+								message = "New channel: #{}  Don't forget to `/invite @simple_bot`!".format(channel["name"])
+							
+							sc.api_call("chat.postMessage", text=message, channel="#general", link_names=1, as_user=True)
 							channels[channel["id"]] = channel["name"]
 						except KeyError as e:
 							print("Key error joining channel")
